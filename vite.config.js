48ccc77f055d62e947110530d3317ecec29d66d9
yashpimpalkarntil <yash.p@netlabindia.com>
2026-06-16 17:30:10 +0530

    fix: update base path in vite.config.js to correct deployment URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ntplsignature/',
+  base: '/ntiplsignature/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
